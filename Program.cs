58930d6b0f4a24b0e575d8bca263b6bbd934e1fd
salvadorjhai <jhai_salvador@yahoo.com>
2025-05-02 15:41:37 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.RateLimiting;
+
+namespace no_as_a_service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateSlimBuilder(args);
+
+            builder.Services.AddRateLimiter(options =>
+            {
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+                {
+                    var ip = httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+
+                    return RateLimitPartition.GetFixedWindowLimiter(ip, partition => new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 120, 
+                        Window = TimeSpan.FromMinutes(1),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0
+                    });
+                });
+            });
+
+            var app = builder.Build();
+            var reasons = JRaw.Parse(File.ReadAllText(@".\reason.js"));
+            var rnd = new Random(DateTime.Now.Second);
+
+            app.MapGet("/", () => "welcome to no-as-a-service, navigate to /no to get started");
+            app.MapGet("/no", () => reasons.ElementAt(rnd.Next(1, reasons.Count()) - 1).ToString());
+
+            app.Run();
+        }
+    }
+
+}
